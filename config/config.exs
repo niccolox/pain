496d36f17cdbcf741182f5ed3c77cacfae79066b
c4lliope <code@assembled.app>
2023-08-09 15:52:50 -0400

    Much progress on using hex `:surface_bulma`.

--- config/config.exs
+++ config/config.exs
@@ -67,3 +67,27 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+config :surface, components: [
+  {Surface.Components.Form.Field, [default_class: "field"]},
+  {Surface.Components.Form.Label, [default_class: "label"]},
+  {SurfaceBulma.Collapsible, propagate_context_to_slots: true},
+  {SurfaceBulma.Dropdown, propagate_context_to_slots: true},
+  {SurfaceBulma.Navbar, propagate_context_to_slots: true},
+  {SurfaceBulma.Navbar.Brand, propagate_context_to_slots: true},
+  {SurfaceBulma.Navbar.Dropdown, propagate_context_to_slots: true},
+  {SurfaceBulma.Form, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.Checkbox, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.Input, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.TextInput, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.PasswordInput, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.InputWrapper, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.InputWrapperTest.Slot, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.InputWrapper, :render_left_addon, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.InputWrapper, :render_right_addon, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.FileInput, propagate_context_to_slots: true},
+  {SurfaceBulma.Form.Select, propagate_context_to_slots: true},
+  {SurfaceBulma.Panel, propagate_context_to_slots: true},
+  {SurfaceBulma.Panel.Tab, propagate_context_to_slots: true},
+  {SurfaceBulma.Panel.Tab.TabItem, propagate_context_to_slots: true}
+]
