2415039c51b50e33d26faab960f5341dd6ed7a48
c4lliope <code@assembled.app>
2023-08-12 00:08:31 -0400

    Keep card width @ 40rem.

--- lib/pain_web/components/card.ex
+++ lib/pain_web/components/card.ex
@@ -16,22 +16,16 @@ defmodule PainWeb.Components.Card do
   @doc "The background color"
   prop rounded, :boolean, default: false
 
-  @doc """
-  The max width.
-  sm: `max-w-sm`, md: `max-w-md`, lg: `max-w-lg`
-  """
-  prop max_width, :string, values: ["sm", "md", "lg"]
-
   def render(assigns) do
     ~F"""
     <style>
-      .card { @apply overflow-hidden shadow-md; }
+      .card { @apply overflow-hidden shadow-md; width: 40rem; }
       .content { @apply px-6 py-4 text-gray-700 text-base; }
       .header { @apply p-6 font-semibold text-2xl text-brand w-full bg-gray-200; }
       .footer { @apply px-6 py-4; }
     </style>
 
-    <div class={"card", "max-w-#{@max_width}", "rounded-2xl": @rounded}>
+    <div class={"card", "rounded-2xl": @rounded}>
       <div class="header">
         <#slot {@header}/>
       </div>
