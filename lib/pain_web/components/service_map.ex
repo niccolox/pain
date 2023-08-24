265f85269ce8708f39abc1626a819d2b5f91d9f4
c4lliope <code@assembled.app>
2023-08-24 11:54:31 -0400

    Render `ServiceMap` nicely on small screens.

--- lib/pain_web/components/service_map.ex
+++ lib/pain_web/components/service_map.ex
@@ -9,27 +9,52 @@ defmodule PainWeb.Components.ServiceMap do
     ~F"""
     <style>
       section { display: flex; justify-content: end; }
+      svg text { fill:#000000; }
+      svg path { stroke-width:2; stroke:#000000; }
+      .backup { display: none; }
+      .backup .service { text-align: right; }
+      .backup .column {
+        display: inline-block;
+        width: 48px;
+        text-align: center;
+        padding-top: 1rem;
+      }
+      @media(max-width: 400px) {
+        svg { display: none; }
+        .backup {
+          padding-top: 1rem;
+          display: flex;
+          flex-direction: column;
+          align-content: end;
+        }
+      }
     </style>
 
     <section class="service-map" >
       <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"
         version="1.1" style="-webkit-print-color-adjust:exact" fill="none"
-        viewBox={"0 0 600 #{40 * (num + 1)}"} width="600" height={40 * (num + 1)}
-      >
-        {#for {n, service} <- @services}
-          <g>
-            <text x={580 - 48 * num} y={40 * n} text-anchor="end"
-            style="fill:#000000;" dominant-baseline="ideographic" >
-              {n}: {service}</text>
-            <path rx="0" ry="0" style="stroke-width:2;stroke:#000000"
-              d={"
-              M #{600 - 48 * num} , #{40 * n - 6}
-              L #{624 - 48 * n}   , #{40 * n - 6}
-              L #{624 - 48 * n}   , #{40 * (num + 1)}
-              "} />
-          </g>
-        {/for}
+        viewBox={"0 0 360 #{40 * (num + 1)}"} width="360" height={40 * (num + 1)}
+      > {#for {n, service} <- @services} <g>
+        <text x={360 - 48 * num} y={40 * n} text-anchor="end"
+         dominant-baseline="ideographic" >
+          {n}: {Squish.pare(service, size: 20)}</text>
+        <path rx="0" ry="0"
+          d={"
+          M #{372 - 48 * num} , #{40 * n - 6}
+          L #{384 - 48 * n}   , #{40 * n - 6}
+          L #{384 - 48 * n}   , #{40 * (num + 1)}
+          "} />
+        </g> {/for}
       </svg>
+
+      <div class="backup">
+        {#for {n, service} <- @services}
+          <span class="service">{n}: {Squish.pare(service, size: 20)}</span>
+        {/for}
+        <div class="columns">
+          {#for {n,_} <- @services}<span class="column">({n})</span>{/for}
+        </div>
+      </div>
     </section>
     """
   end
--- lib/squish.ex
+++ lib/squish.ex
@@ -2,4 +2,17 @@ defmodule Squish do
   def squish([]), do: []
   def squish([head | tail]) when is_list(head), do: head ++ squish(tail)
   def squish([head | tail]), do: [head] ++ squish(tail)
+
+  def pare(text, opts \\ []) do
+    size = opts[:size] || 50
+    omission = opts[:omission] || "..."
+
+    cond do
+      not String.valid?(text) -> text
+      String.length(text) < size -> text
+      true ->
+        length_with_omission = size - String.length(omission)
+        "#{String.slice(text, 0, length_with_omission)}#{omission}"
+    end
+  end
 end
