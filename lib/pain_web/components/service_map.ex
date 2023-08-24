79cf5f5cf410e8ff92ab09a8968032a4f324507b
c4lliope <code@assembled.app>
2023-08-24 12:31:19 -0400

    Color and braid `<ServiceMap/>` threads.

--- lib/pain_web/components/service_map.ex
+++ lib/pain_web/components/service_map.ex
@@ -28,21 +28,27 @@ defmodule PainWeb.Components.ServiceMap do
           align-content: end;
         }
       }
+
+      g.num-1 text {fill: green;  } g.num-1 path {stroke: green;  }
+      g.num-2 text {fill: blue;   } g.num-2 path {stroke: blue;   }
+      g.num-3 text {fill: purple; } g.num-3 path {stroke: purple; }
+      g.num-4 text {fill: red;    } g.num-4 path {stroke: red;    }
     </style>
 
     <section class="service-map" >
       <svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"
         version="1.1" style="-webkit-print-color-adjust:exact" fill="none"
         viewBox={"0 0 360 #{40 * (num + 1)}"} width="360" height={40 * (num + 1)}
-      > {#for {n, service} <- @services} <g>
+      > {#for {n, service} <- @services}
+      <g class={"num-#{n}"}>
         <text x={360 - 48 * num} y={40 * n} text-anchor="end"
          dominant-baseline="ideographic" >
           {n}: {Squish.pare(service, size: 20)}</text>
         <path rx="0" ry="0"
           d={"
-          M #{372 - 48 * num} , #{40 * n - 6}
-          L #{384 - 48 * n}   , #{40 * n - 6}
-          L #{384 - 48 * n}   , #{40 * (num + 1)}
+          M #{372 - 48 * num}       , #{40 * n - 6}
+          L #{384 - 48 * (num-n+1)} , #{40 * n - 6}
+          L #{384 - 48 * (num-n+1)} , #{40 * (num+1)}
           "} />
         </g> {/for}
       </svg>
